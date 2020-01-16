faccb69413da4e0560d3953b06f2f2ee76d22b78
zashuvro <mdshuvro0183@gmail.com>
2020-01-15 19:34:35 -0500

    change the sort class

--- src/algorithm/Sort.java
+++ src/algorithm/Sort.java
@@ -5,7 +5,7 @@ public class Sort {
     long executionTime = 0;
 	/*
 	 * Please implement all the sorting algorithm. Feel free to add helper methods.
-	 * Store all the sorted data into one of the databases.
+	 * Store all the sorted data into one of the databases..
 	 */
 
 
