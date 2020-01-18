bf520653ee41b13892967a5c9ae2c09401645049
zashuvro <mdshuvro0183@gmail.com>
2020-01-18 05:36:56 -0500

    mathProblems package MakePyramid class

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -13,6 +13,16 @@ public class MakePyramid {
                          * * * * * *
 
         */
+        for(int i=0;i<6;i++){
+
+            for(int k=1;k<6-i;k++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
 
         }
