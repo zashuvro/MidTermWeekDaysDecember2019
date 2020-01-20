83a8f47f04e6099988d46cff22d4dcd9f04cf74f
zashuvro <mdshuvro0183@gmail.com>
2020-01-19 20:55:41 -0500

    stringProblems package duplicateWord class done

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,6 +1,8 @@
 package string.problems;
 
 
+import java.util.*;
+
 public class DuplicateWord {
 
     public static void main(String[] args) {
@@ -9,8 +11,32 @@ public class DuplicateWord {
          * Also Find the average length of the words.
          */
 
-        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language.";
+    duplicateWord(st);
+    }
 
+    public static String duplicateWord(String s){
+        List<String > list=Arrays.asList(s.toLowerCase().split(" "));
+        Set<String> duplicate=new HashSet<>(list);
+        System.out.println(s);
+
+        for(String word:duplicate)
+        System.out.println("<"+word+">"+" :word in String "+ Collections.frequency(list, word)+" times");
+
+
+        char [] ch=s.toCharArray();
+        String [] word=s.split(" ");
+        double space=0;
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]==' '){
+                space++;
+            }
+        }
+
+        System.out.println("The Total Character of The String st is: "+(double)(ch.length-space));
+        System.out.println("Average length of the words:: "+ (double)((ch.length-space)/word.length));
+
+        return s;
     }
 
 }
