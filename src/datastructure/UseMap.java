919484116aa0e08c11f4429214a82c53f287191a
zashuvro <mdshuvro0183@gmail.com>
2020-01-20 05:39:56 -0500

    useMap class done

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.*;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,6 +12,39 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
+		List<String> name = new ArrayList<>();
+		name.add("Abir");
+		name.add("Zahid");
+		name.add("jon");
+
+		List<String> color = new ArrayList<>();
+		color.add("Red");
+		color.add("Black");
+		color.add("White");
+
+
+
+
+		List<Integer> id = new ArrayList<>();
+		id.add(1);
+		id.add(2);
+		id.add(3);
+		Map<String,List<String>> list1 =new LinkedHashMap<String, List<String>>();
+		list1.put("name",name);
+		list1.put("color", color);
+
+
+		Map<String,List<Integer>> list2 =new HashMap<String, List<Integer>>();
+		list2.put("ID", id);
+
+
+		for(Map.Entry key: list1.entrySet()){
+			System.out.println(key.getKey()+" "+key.getValue());
+		}
+
+		for(Map.Entry key: list2.entrySet()){
+			System.out.println(key.getKey()+" "+key.getValue());
+		}
 
 	}
 
