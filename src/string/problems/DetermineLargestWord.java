94a45e8b3bf7ad700e5bacf8895cfa99caf4fcd9
zashuvro <mdshuvro0183@gmail.com>
2020-01-19 19:22:45 -0500

    stringProblems package DeterminelargestWord class done

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -1,7 +1,7 @@
 package string.problems;
 
+
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class DetermineLargestWord {
@@ -13,14 +13,22 @@ public class DetermineLargestWord {
         String s="Human brain is a biological learning machine";
         Map<Integer, String> wordNLength = findTheLargestWord(s);
         //implement
-
+        for(Map.Entry<Integer,String > entry:wordNLength.entrySet()) {
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
     }
 
     public static Map<Integer, String> findTheLargestWord(String wordGiven){
         Map<Integer, String> map = new HashMap<Integer, String>();
         String st = "";
         //implement
-
+        String [] word=wordGiven.split(" ");
+        for(int i=0; i<word.length;i++) {
+            if (word[i].length() >=st.length()){
+                st=word[i];
+            }
+        }
+        map.put(st.length(),st);
         return map;
     }
 }
