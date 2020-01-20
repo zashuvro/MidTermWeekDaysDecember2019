75362f3cd4380e51952f7fbd8cf61e02f88a3d79
zashuvro <mdshuvro0183@gmail.com>
2020-01-20 05:42:25 -0500

    useQueue class done

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,6 +12,25 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+		Queue<String> color = new LinkedList<>();
+		color.add("Red");
+		color.add("Black");
+		color.add("Yellow");
+		color.add("White");
+		System.out.println("peek: "+color.peek());
+		System.out.println("size: "+color.size());
+		System.out.println("remove: "+color.remove());
+		System.out.println("peek: "+color.peek());
+		System.out.println("poll: "+color.poll());
+		for(String x : color ){
+			System.out.println("for each loop : "+x);
+		}
+		Iterator it = color.iterator();
+		while(it.hasNext()){
+			System.out.println("Iterator : "+it.next());
+		}
+
+
 
 	}
 
