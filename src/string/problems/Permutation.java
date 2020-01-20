e65b4629b0af2cba3402028d81cfc0d223e9edd9
zashuvro <mdshuvro0183@gmail.com>
2020-01-20 00:17:32 -0500

    stringProblems package permutuion class done

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -10,5 +10,23 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+        printPermutn("abc","");
+    }
+
+    public static void printPermutn(String str, String ans)
+    {
+
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            String ros = str.substring(0, i) +
+                    str.substring(i + 1);
+            printPermutn(ros, ans + ch);
+        }
     }
 }
